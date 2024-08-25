d125294b228b477e563d287e058a9a5b9c05ccc8
kozochkina82 <kozochkina82@yandex.ru>
2024-08-25 19:45:25 +0500

    feat: make work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+const { test } = require("mocha");
+
 module.exports = function reverse (n) {
-  
+ let str = Math.abs(n).toString();
+ let i = 0;
+  let result = '';
+  while (i < str.length) {
+  result = `${str[i]}${result}`;
+   i = i + 1;
+  }
+return result;
 }
