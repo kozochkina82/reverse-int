73918ffcaee57a3186c588418f48937e5d1c75be
kozochkina82 <kozochkina82@yandex.ru>
2026-05-06 16:35:00 +0200

    fix: fix linter bugs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const str = Math.abs(n).toString();
+  let i = 0;
+  let result = '';
+  while (i < str.length) {
+    result = `${str[i]}${result}`;
+    i += 1;
+  }
+  return result;
 };
